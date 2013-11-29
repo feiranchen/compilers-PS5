71f308c95dc63353272d737b231e671355e7147d
Nikash <nn239@cornell.edu>
2013-11-28 19:02:29 -0500

    input fix

--- src/cubex_external_functions.c
+++ src/cubex_external_functions.c
@@ -7,6 +7,8 @@ static int curr_input = 1;
 static int input_len;
 static char** input;
 
+extern int nmalloc;
+extern int nfree;
 
 void initialize(int argc, char** argv) {
   input_len = argc;
@@ -42,12 +44,12 @@ void read_line(char* buffer) {
 }
 
 void* x3malloc(int size) {
-
+  nmalloc++;
   return malloc(size);
 }
 
 void x3free(void* ptr) {
-
+  nfree++;
   free(ptr);
 }
 
